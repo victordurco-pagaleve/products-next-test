cc7fbd8b21856f5dee5c3f9ec65a3bd575d1ecdd
Victor Durço <victor.durco@pagaleve.com.br>
2025-12-18 13:47:24 -0300

    feat: add nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import Link from "next/link";
 
 const quicksand = Quicksand({
   variable: "--font-quicksand",
@@ -23,6 +24,13 @@ export default function RootLayout({
       <body
         className={`${quicksand.variable} antialiased`}
       >
+        <nav className="flex justify-center gap-4 border-b-2 bg-secondary">
+          <Link href="/products/client-side" className="border-b-2 border-transparent hover:border-primary">Client-Side</Link>
+          <div className="w-1 h-6 bg-primary inline-block"></div>
+          <Link href="/products/server-side" className="border-b-2 border-transparent hover:border-primary">Server-Side</Link>
+          <div className="w-1 h-6 bg-primary inline-block"></div>
+          <Link href="/products/server-actions" className="border-b-2 border-transparent hover:border-primary">Server Actions</Link>
+        </nav>
         <Providers>{children}</Providers>
       </body>
     </html>
